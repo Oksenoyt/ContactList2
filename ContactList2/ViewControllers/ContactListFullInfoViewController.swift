d43862e88fb2d6022deab187d23061b45cd19681
Oksenoyt <a.oksenoyt@gmail.com>
2022-07-22 13:23:18 +0300

    fix ContactListFullInfoVC

--- ContactList2/ViewControllers/ContactListFullInfoViewController.swift
+++ ContactList2/ViewControllers/ContactListFullInfoViewController.swift
@@ -16,20 +16,24 @@ class ContactListFullInfoViewController: UITableViewController {
         contactList.count
     }
     
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        contactList[section].fullName
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         2
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let contact = contactList.randomElement()
         let cell = tableView.dequeueReusableCell(withIdentifier: "contactFullInfo", for: indexPath)
+        let contact = contactList[indexPath.section]
         var content = cell.defaultContentConfiguration()
         
         if indexPath.row == 0 {
-            content.text = contact?.phoneNumber
+            content.text = contact.phoneNumber
             content.image = UIImage(systemName: "phone")
         } else {
-            content.text = contact?.email
+            content.text = contact.email
             content.image = UIImage(systemName: "mail")
         }
         
@@ -37,7 +41,7 @@ class ContactListFullInfoViewController: UITableViewController {
         return cell
     }
     
-    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        contactList[section].fullName
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
